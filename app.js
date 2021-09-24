2071818c4bad3e28220c17860c25ba7f12c38ebf
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-09-24 09:31:55 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+require('colors');
+const guardarDB = require('./helpers/guardarArcchivo');
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
+
+const main = async () => {
+  console.log('Hola Mundo');
+
+  let opt = '';
+  const tareas = new Tareas();
+
+  do {
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case '1':
+        // crear opcion
+        const desc = await leerInput('Descripcion:');
+        tareas.crearTarea(desc);
+        break;
+
+      case '2':
+        console.log(tareas.listadoArr);
+        break;
+    }
+
+    // guardarDB(tareas.listadoArr);
+
+    await pausa();
+  } while (opt !== '0');
+};
+
+main();
---
+++ models/tareas.js
@@ -0,0 +1,26 @@
+const Tarea = require('./tarea');
+
+class Tareas {
+  constructor() {
+    this._listado = {};
+  }
+
+  get listadoArr() {
+    const listado = [];
+
+    Object.keys(this._listado).forEach((key) => {
+      const tarea = this._listado[key];
+      listado.push(tarea);
+    });
+
+    return listado;
+  }
+
+  crearTarea(desc = '') {
+    const tarea = new Tarea(desc);
+
+    this._listado[tarea.id] = tarea;
+  }
+}
+
+module.exports = Tareas;
