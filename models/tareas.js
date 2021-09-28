6c9580128c82c882ede824400b82867aba191f91
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-09-28 18:28:50 -0400

    Tareas completadas y pendientes | opciones del menu

--- models/tareas.js
+++ models/tareas.js
@@ -30,34 +30,37 @@ class Tareas {
   }
 
   listadoCompleto() {
-    this.recorrerArr(this.listadoArr);
-  }
-
-  recorrerArr(tareas) {
     console.log();
-    tareas.forEach((tarea, i) => {
-      const idx = `${i + 1}`.green;
+    this.listadoArr.forEach((tarea, i) => {
+      const idx = `${i + 1}.`.green;
       const { desc, completadoEn } = tarea;
       const estado = completadoEn ? 'Completada'.green : 'Pendiente'.red;
 
-      console.log(`${idx}. ${desc} :: ${estado}`);
+      console.log(`${idx} ${desc} :: ${estado}`);
     });
   }
 
   listarPendienteCompletadas(completadas = true) {
-    if (completadas) {
-      const completadasArr = this.listadoArr.filter(
-        (tarea) => tarea.completadoEn !== null
-      );
+    console.log();
+    let contador = 0;
+    this.listadoArr.forEach((tarea) => {
+      const { desc, completadoEn } = tarea;
+      const estado = completadoEn ? 'Completada'.green : 'Pendiente'.red;
 
-      this.recorrerArr(completadasArr);
-    } else {
-      const pendientesArr = this.listadoArr.filter(
-        (tarea) => tarea.completadoEn === null
-      );
-
-      this.recorrerArr(pendientesArr);
-    }
+      if (completadas) {
+        // Mostrar completadas
+        if (completadoEn) {
+          contador += 1;
+          console.log(`${(contador + '.').green} ${desc} :: ${completadoEn}`);
+        }
+      } else {
+        // Mostrar pendientes
+        if (!completadoEn) {
+          contador += 1;
+          console.log(`${(contador + '.').green} ${desc} :: ${estado}`);
+        }
+      }
+    });
   }
 }
 
